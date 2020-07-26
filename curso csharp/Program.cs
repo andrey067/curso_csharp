7ea37844387b3caaddb73b206802d02ae1b0cd2c
andrey067 <audrey.ernesto.lima@gmail.com>
2020-07-25 20:21:37 -0400

    Teste commit

---
+++ curso csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace curso_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
